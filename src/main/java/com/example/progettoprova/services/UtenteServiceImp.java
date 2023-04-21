2d340138ecfab4b8227e4791304ba49111d0d7fd
11Nait11 <115213428+11Nait11@users.noreply.github.com>
2023-04-21 19:47:52 +0200

    user dto-service-controller testati end point aggiunto springdoc

--- src/main/java/com/example/progettoprova/services/UtenteServiceImp.java
+++ src/main/java/com/example/progettoprova/services/UtenteServiceImp.java
@@ -18,21 +18,50 @@ public class UtenteServiceImp implements UtenteService{
     private final UtenteDao utenteDao;
     private final ModelMapper modelMapper;
 
+    private void creaDb(){
+        Utente u1=new Utente();
+        u1.setFirstName("Paperino");
+        u1.setLastName("Bianchi");
+        utenteDao.save(u1);
+
+        Utente u2=new Utente();
+        u2.setFirstName("Archimede");
+        u2.setLastName("Rossi");
+        utenteDao.save(u2);
+    }
+
     @Override
     public List<UtenteDto> dammiUtenti(){
-//        Utente u1=new Utente();
-//        u1.setFirstName("Paperino");
-//        u1.setLastName("Bianchi");
-//        utenteDao.save(u1);
-//
-//        Utente u2=new Utente();
-//        u2.setFirstName("Archimede");
-//        u2.setLastName("Rossi");
-//        utenteDao.save(u2);
-
+//        creaDb();
         List<Utente> utenti = utenteDao.findAll();
         if(utenti.isEmpty())
             throw new EntityNotFoundException("Utenti non trovati!");
         return utenti.stream().map(u->modelMapper.map(u, UtenteDto.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public UtenteDto dammiUtente(Long id) {
+        creaDb();
+        System.out.println("ServiceDammiUtente");
+        Utente utente=utenteDao.findById(id).orElseThrow(()-> new EntityNotFoundException("Id non Presente"));
+        if(utente!=null){
+            System.out.println(utente.getFirstName());
+        }
+
+        return modelMapper.map(utente,UtenteDto.class);
+    }
+
+    @Override
+    public void salva(Utente u) {utenteDao.save(u);}
+
+    @Override
+    public void salvaDto(UtenteDto uD) {
+        Utente u= modelMapper.map(uD, Utente.class);
+        utenteDao.save(u);
+    }
+
+    @Override
+    public void cancella() {
+
     }
 }
